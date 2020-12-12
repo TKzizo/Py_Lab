f6309ecafc173936ac58ceef2d4cc8b028bbe6ff
TKzizo <ia_takouche@esi.dz>
2020-12-12 00:32:24 +0000

    fixed hangman

--- project3/5_hangman.py
+++ project3/5_hangman.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python
 
-
 import string 
-# get a new word list 
-
+import random
+from words import listofwords
 
 def valid_word():
-	word = # select from the word list
-	while "-" in word or " " in word:
-		word = # select from word list 
+	word = random.choice(listofwords).upper()
+	while ("-" in word) or (" " in word):
+		word = random.choice(listofwords)
 	return word 
 
 def hangman(x):
 	word = x
-	word_letters = set(x)
+	word_letters = set(x)	
 	alphabet = set(string.ascii_uppercase)
-	used_letters = set() # already guessed letters 
+	used_letters = set() 
 
-	while len(word_letters > 0):
+	while len(word_letters) > 0:
 
 		print("Used letter: ", " ".join(used_letters))
 		word_list = [letter if letter in used_letters else "-" for letter in word]
-		print(" ".join(used_letters))
+		print("current word: "," ".join(word_list))
 
 		user_letter = input("guess a letter: ")
-		if user_letter in alphabet - used_letters:
+		if (user_letter in alphabet - used_letters):
 			used_letters.add(user_letter)
-			if user_letter in word_letters:
-				word_letter.remove(user_letter)
-		elif user_letter in used_letter:
+			if (user_letter in word_letters):
+				word_letters.remove(user_letter)
+		elif (user_letter in used_letters):
 			print("you already used that letter ")
 		else: 
 			print("that's invalid character")
-		
-
+	print("Congrats you found the word !!!!")
+	input()	
+	
 hangman(valid_word())
-
